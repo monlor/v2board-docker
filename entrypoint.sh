92f4fb6532419fa19dc507a86b9d453826e601dd
monlor <me@monlor.com>
2023-05-07 12:12:06 +0800

    首次启动提示更凸显

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,11 +12,13 @@ ${HOME_URL:-http://localhost} {
 EOF
 
 cat <<-EOF
+============================================
 首次启动时需要执行以下操作：
 1. 进入容器：docker exec -it v2board /bin/bash
 2. 初始化数据库：php artisan v2board:install
 3. 启动服务：php artisan horizon &
 4. 退出容器：exit
+============================================
 EOF
 
 /usr/bin/supervisord -c /etc/supervisor/conf.d/supervisord.conf
